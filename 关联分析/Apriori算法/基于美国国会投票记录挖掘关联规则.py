609a8925fe0ead2dc2d1c9b41b8e1d60ca1bb074
yyf <haibiyu@163.com>
2020-04-29 19:25:11 +0800

    提交关联分析代码和数据集

---
+++ 关联分析/Apriori算法/基于美国国会投票记录挖掘关联规则.py
@@ -0,0 +1,11 @@
+# !/usr/bin/env python    
+# -*-coding:utf-8 -*-
+
+"""
+# File       : 基于美国国会投票记录挖掘关联规则.py
+# Time       ：2020/3/19 16:56 
+# Author     ：Yan You Fei
+# version    ：python 3.6
+# Description：
+"""
+from votesmart import votesmart
